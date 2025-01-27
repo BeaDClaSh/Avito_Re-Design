f0d373dfeb88457ec1d052800d83c6de593949c6
AHUENEN2008 <ss576285@gmail.com>
2025-01-27 22:29:04 +0100

    Refactor(ItemComponent): trying to fix errors in test deploy try 4

--- src/app/messages/FirstUserID/[userID]/NextUserId/[nextUserId]/page.tsx
+++ src/app/messages/FirstUserID/[userID]/NextUserId/[nextUserId]/page.tsx
@@ -1,18 +1,18 @@
+"use client"
 import React from "react";
+import {useParams} from "next/navigation";
 
-interface ItemPageProps{
-    params:{
-        userId:string,
-        nextUserId:string,
-    }
-}
 
-const Message1to1:React.FC<ItemPageProps> = ({params}) => {
+const Message1to1 = () => {
+const params=useParams();
+
+const userId=params.userId as string;
+    const nextUserId=params.nextUserId as string;
 
     return (
         <div>
-            <h1>{params.userId}</h1>
-            <h1>{params.nextUserId}</h1>
+            <h1>{userId}</h1>
+            <h1>{nextUserId}</h1>
         </div>
     )
 }
